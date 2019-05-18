32f1ea9a2d685581885d5e0ab8515f0cc4672b95
Andrew Crawford <code-in-time@hotmail.com>
2019-05-18 18:14:05 +0200

    add input to imiy from

--- src/components/Countainer.vue
+++ src/components/Countainer.vue
@@ -4,10 +4,10 @@
     <button v-on:click="addMe" type="button">click + </button>
     <button v-on:click="xCounter++" type="button">click2 + </button>
     <h1>s: {{this.personName}}</h1>
-    <button class="btnSetName" v-on:click="() => $emit('emit-setName', 'nnnnnnnnnn')">setName</button>
+    <button class="btnSetName" v-on:click="() => $emit('emit-setName', this.inpValue)">setName</button>
+    <input type="text" v-on:keyup="getTextInput" v-model="inpValue" />
   </div>
 </template>
-
 <script>
 export default {
     name: 'Counter',
@@ -17,13 +17,18 @@ export default {
     },
     data() {
       return {
-        xCounter: 0
+        xCounter: 0,
+        inpValue: 'g'
       }
     },
     methods: {
       addMe(e) {
         console.log(e)
         this.xCounter++
+      },
+      getTextInput(e) {
+        console.log('getTextInput', e)
+        this.$emit('emit-setName', e.target.value)
       }
     }
 
